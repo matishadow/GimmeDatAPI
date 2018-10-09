c2aa8c82da934787a591df4c3108ac8db208ac7a
matishadow <matishadow@gmail.com>
2018-10-09 20:58:50 +0200

    Add POCOs

---
+++ GimmeDatAPI.PlainOldClrObjects/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GimmeDatAPI.PlainOldClrObjects
+{
+    public class Menu
+    {
+        public string Soup { get; set; }
+        public string DeluxeMeal { get; set; }
+        
+        public IEnumerable<string> MealsOfTheDay { get; set; }
+    }
+}
---
+++ GimmeDatAPI.PlainOldClrObjects/ZaścianekData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GimmeDatAPI.PlainOldClrObjects
+{
+    public class ZaścianekData
+    {
+        public DateTime CurrentDateTime { get; set; }
+        public DateTime MenuDate { get; set; }
+
+        public Menu Menu { get; set; }
+    }
+}
