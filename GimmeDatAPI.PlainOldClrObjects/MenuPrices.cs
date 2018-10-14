f130bcf14d99620d24725b1771e0550ffd21da59
matishadow <matishadow@gmail.com>
2018-10-14 20:54:31 +0200

    Add POCOs for prices

---
+++ GimmeDatAPI.PlainOldClrObjects/MenuPrices.cs
@@ -0,0 +1,9 @@
+namespace GimmeDatAPI.PlainOldClrObjects
+{
+    public class MenuPrices
+    {
+        public string SoupPrice { get; set; }
+        public string StandardMealPrice { get; set; }
+        public string DeluxeMealPrice { get; set; }
+    }
+}
---
+++ GimmeDatAPI.PlainOldClrObjects/ZascianekDataWithPrices.cs
@@ -0,0 +1,7 @@
+namespace GimmeDatAPI.PlainOldClrObjects
+{
+    public class ZascianekDataWithPrices : ZascianekData
+    {
+        private MenuPrices MenuPrices { get; set; }
+    }
+}
